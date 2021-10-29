47148d4f830c159365066de6031dbad13dc33df0
Mutlucan Tokat <6348274-mutlucan@users.noreply.gitlab.com>
2021-10-29 06:44:55 +0300

    Enable CORS for localhost:3000 which is default React development runserver port

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from functools import lru_cache
 from typing import Dict
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from app.config import settings, logging
@@ -13,6 +14,18 @@ logger = logging.getLogger(__name__)
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
